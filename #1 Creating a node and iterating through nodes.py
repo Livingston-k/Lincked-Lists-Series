cceb5f2b855ecdce29b27c75bfabadb99bcc8a5b
Kaddu Livingstone <kaddulivingston@gmail.com>
2021-08-04 01:03:55 +0300

    Add files via upload

---
+++ #1 Creating a node and iterating through nodes.py
@@ -0,0 +1,19 @@
+class Node:
+    def __init__(self):
+        self.data = data
+        selft.next = ""
+
+class LinckedList:
+    def __init__(self):
+        self.head = ""
+    def interator(self):
+        if self.head == "":
+            print("Lincked List is Empty")
+        nn = self.head
+        while nn !="":
+            print(nn.data)
+            nn = nn.next
+
+ll = LinckedList()
+ll.interator()
+            
